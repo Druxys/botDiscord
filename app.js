1716a6f3c97c95e015bd407ac688bf551bc842c1
Paul Turpin <17347@csmrouen.net>
2021-10-11 12:09:24 +0200

    fix app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const commands = [{
     name: 'ping',
@@ -13,7 +15,7 @@ const rest = new REST({ version: '9' }).setToken('token');
         console.log('Started refreshing application (/) commands.');
 
         await rest.put(
-            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            Routes.applicationGuildCommands('897062168820854784', 'd4a06310c76f501ba0576ed6e71ecd69ab800dd05be5f9e3e052868104af0c0b'),
             { body: commands },
         );
 
@@ -21,4 +23,18 @@ const rest = new REST({ version: '9' }).setToken('token');
     } catch (error) {
         console.error(error);
     }
+
+
 })();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
