be5a6767fa4a3bfb9662a7d594b59c5bea026fb3
Paul Turpin <17347@csmrouen.net>
2021-10-11 13:34:48 +0200

    add the first method to test discord.js

--- app.js
+++ app.js
@@ -1,40 +1,24 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const DiscordJs = require('discord.js');
+const dotenv = require('dotenv');
+const {Intents} = require("discord.js");
 
-const commands = [{
-    name: 'ping',
-    description: 'Replies with Pong!'
-}];
+dotenv.config();
 
-const rest = new REST({ version: '9' }).setToken('token');
-
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-
-        await rest.put(
-            Routes.applicationGuildCommands('897062168820854784', 'd4a06310c76f501ba0576ed6e71ecd69ab800dd05be5f9e3e052868104af0c0b'),
-            { body: commands },
-        );
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-
-
-})();
-
+const client = new DiscordJs.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+    ]
+});
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log('ayé')
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong'
+        })
     }
-});
+})
+client.login(process.env.TOKEN);
